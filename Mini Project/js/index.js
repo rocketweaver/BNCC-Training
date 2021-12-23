1337f2fdb16f45e27d7ee5198475e4a2c9907522
Arbhy Adityabrahma <arbhyaditya2@gmail.com>
2021-12-23 20:33:50 +0700

    fixing some bugs and variable names to more proper ones

--- Mini Project/js/index.js
+++ Mini Project/js/index.js
@@ -51,16 +51,15 @@ let userInput = {};
 let genderValue = () => {
     for (var i = 0, length = gender.length; i < length; i++) {
         if (gender[i].checked) {
-          // do whatever you want with the checked radio
           return gender[i].value;
-      
-          // only one radio can be logically checked, don't check the rest
           break;
         }
       }
 }
 
 submit.addEventListener("click", () => {
+    inputValidation();
+    
     userInput = {
         name : firstName.value + " " + lastName.value,
         email : email.value,
@@ -71,3 +70,60 @@ submit.addEventListener("click", () => {
 
     console.log(userInput);
 });
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const inputValidation = () => {
+    const nameGroup = document.querySelectorAll(".name-input");
+    const nameErrorMsg = document.getElementById("error-name");
+    const firstNameValue = firstName.value.trim();
+
+    if(firstNameValue === "") {
+        nameGroup.forEach(e => {
+            e.classList.add("error-border");
+        });
+        nameErrorMsg.innerHTML = "Please input your first name."
+    } else {
+        nameGroup.forEach(e => {
+            e.classList.add("success-border");
+        });
+        nameErrorMsg.innerHTML = "";
+    }
+
+    const emailValue = email.value.trim();
+    const emailErrorMsg = document.getElementById("error-email");
+
+    if(emailValue.length === "") {
+        email.classList.add("error-border");
+        emailErrorMsg.innerHTML = "Please input your email.";
+    } else if(!isValidEmail(emailValue)) {
+        email.classList.add("error-border");
+        emailErrorMsg.innerHTML = "Please use correct format on your email.";
+    } else {
+        email.classList.add("success-border");
+        emailErrorMsg.innerHTML = "";
+    }
+
+    const phoneNumberValue = phoneNumber.value.trim();
+    const phoneNumberErrorMsg = document.getElementById("error-phone-number"); 
+    if(phoneNumberValue.length < 10 || phoneNumberValue.length > 12) {
+        phoneNumber.classList.add("error-border");
+        phoneNumberErrorMsg.innerHTML = "The phone number can't be less than 10 or more than 12.";
+    } else {
+        phoneNumber.classList.add("success-border");
+        phoneNumberErrorMsg.innerHTML = "";
+    }
+
+    const msgLength = msg.value.split(' ').length;
+    const msgErrorMsg = document.getElementById("error-msg"); 
+    if(msgLength < 5) {
+        msg.classList.add("error-border");
+        msgErrorMsg.innerHTML = "The message can't be less than 5 words";
+    } else {
+        msg.classList.add("success-border");
+        msgErrorMsg.innerHTML = "";
+    }
+};
